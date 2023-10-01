83f1855ffba97222c99042d382343b7065480ea0
M-adhitya235 <126459544+M-adhitya235@users.noreply.github.com>
2023-10-01 20:28:51 +0800

    Update Peta.dart

--- Peta.dart
+++ Peta.dart
@@ -1,57 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart'; 
-
+import 'package:latlong2/latlong.dart';
 
 class Petakrb extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     var marker = <Marker>[];
 
     marker = [
       Marker(
-            point: LatLng(-1.134579, 116.856312),
-            builder: (ctx) => Icon(Icons.pin_drop, color: Colors.green),
-            ),
+        point: LatLng(-1.134579, 116.856312),
+        builder: (ctx) => Icon(
+          Icons.pin_drop,
+          color: Colors.green,
+          size: 30,
+        ),
+      ),
       Marker(
-            point: LatLng(-1.133208, 116.855357),
-            builder: (ctx) => Icon(Icons.pin_drop, color: Colors.blue),
-            ),
+        point: LatLng(-1.133208, 116.855357),
+        builder: (ctx) => Icon(
+          Icons.pin_drop,
+          color: Colors.blue,
+          size: 30,
+        ),
+      ),
       Marker(
-            point: LatLng(-1.133351, 116.855751),
-            builder: (ctx) => Icon(Icons.pin_drop, color: Colors.orange),
-            ),
-       Marker(
-            point: LatLng(-1.134190, 116.855873),
-            builder: (ctx) => Icon(Icons.pin_drop, color: Colors.yellow),
-            ),
-      
+        point: LatLng(-1.133351, 116.855751),
+        builder: (ctx) => Icon(
+          Icons.pin_drop,
+          color: Colors.yellow,
+          size: 30,
+        ),
+      ),
+      Marker(
+        point: LatLng(-1.134190, 116.855873),
+        builder: (ctx) => Icon(
+          Icons.pin_drop,
+          color: Colors.orange,
+          size: 30,
+        ),
+      ),
     ];
 
     return Scaffold(
       body: Center(
-        child: Container(
-          child : Column(
-            children: [
-              Flexible(
-                child: FlutterMap(
-                  options: 
-                  MapOptions(center: LatLng(-1.13392, 116.85521), zoom:18),
-                  layers: [TileLayerOptions(
-                    urlTemplate: 
-                       "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                    subdomains: ['a', 'b', 'c']),
-                    MarkerLayerOptions(
-                      markers: marker,
-                    ),
-                  ],
-                )
-              )
-            ]
-          )
-        )
-      ),
+          child: Container(
+              child: Column(children: [
+        Flexible(
+            child: FlutterMap(
+          options: MapOptions(center: LatLng(-1.13392, 116.85521), zoom: 18),
+          layers: [
+            TileLayerOptions(
+                urlTemplate:
+                    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                subdomains: ['a', 'b', 'c']),
+            MarkerLayerOptions(
+              markers: marker,
+            ),
+          ],
+        ))
+      ]))),
     );
   }
 }
